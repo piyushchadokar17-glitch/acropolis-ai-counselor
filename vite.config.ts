420839cac97db5a6b20699e6f52a42523ce137c3
piyushchadokar17-glitch <piyushchadokar17@gmail.com>
2026-06-07 12:15:49 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    server: { entry: "server" },
+    server: { entry: "start" },
   },
   nitro: {
     preset: "node",
